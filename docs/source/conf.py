cc426cc75b21283ae0f2895ad09ef7e13fe96884
Maximilian Zienecker <m.zienecker@stud.uni-goettingen.de>
2025-04-30 17:46:54 +0000

    First pages

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ngsxditto'
+copyright = '2025, ditto'
+author = 'ditto'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_rtd_theme",
+    "nbsphinx",
+    "sphinx_mdinclude",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+master_doc = "contents"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
