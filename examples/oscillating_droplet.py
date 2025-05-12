1c4408e66d957eee3e9c96ab188d74904b5a3c60
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2025-05-12 13:23:26 +0200

    dummy pseudo-like code

--- examples/oscillating_droplet.py
+++ examples/oscillating_droplet.py
@@ -9,20 +9,23 @@ mesh = Mesh(unit_square.GenerateMesh(maxh=0.1))
 
 #%%
 from ngsxditto.transport import *
-from ngsxditto.meancurv import *
+from ngsxditto.levelset import *
+from ngsxditto.redistancing import *
+from ngsxditto.fluid import *
 
 
 t = 0
 tend = 1
 dt = 0.01
 
-fluid_params = FluidParameters(nu=1e-3) # dictionary of default fluid parameters + changes from arguments
+fluid_params = FluidParameters(viscosity=1e-3) # dictionary of default fluid parameters + changes from arguments (dynamic_viscosity, kinematic_viscosity, density, surface_tension_coeff)
+wall_params = WallParameters(friction_coeff=..., contact_angle=...)
 
-fluid = H1ConformingFluid(mesh, order, fluid_params, levelset=None) # inherited from FluidDiscretization
+fluid = H1ConformingFluid(mesh, order, fluid_params, levelset=None, wall_params=None) # inherited from FluidDiscretization
 
-transport = ExplicitDGTransport(mesh, dt=0.01)
+transport = ExplicitDGTransport(mesh, dt=0.01) # different other options...
 
-redistancing = FastMarching(transport.field) # inherited from Redistancing
+redistancing = FastMarching(transport.field) # inherited from Redistancing / can be None
 
 levelset = LevelSetGeometry(transport, redistancing) # holds LevelSetMeshAdaptation & CutInfo 
 
