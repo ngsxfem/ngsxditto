b8f2c85fd180990ff0e22d2751d6835ac35eb67e
Mika Meyer <mika_meyer@gmx.de>
2025-05-27 17:13:11 +0200

    added test for simple propagation

---
+++ tests/test_implicitsupg.py
@@ -0,0 +1,42 @@
+from ngsxditto.transport import *
+from ngsolve import *
+from netgen.geom2d import SplineGeometry
+from xfem.lsetcurv import *
+
+
+# Example: Rotating circle
+domain = SplineGeometry()
+domain.AddCircle((0,0), 1)
+mesh = Mesh(domain.GenerateMesh(maxh=0.2))
+wind = CF((-y, x))
+t = Parameter(0)
+true_circle = ((x - 1/2 * sin(t))**2 + (y + 1/2 * cos(t))**2)**(1/2) - 1/2
+
+T_end = 1
+dt = 0.02
+
+
+def test_propagation():
+    transport = ImplicitSUPGTransport(mesh, wind, inflow_values=None, dt=dt, order=2)
+    transport.time = t
+    t.Set(0)
+    transport.SetInitialValues(true_circle)
+
+    while transport.time < T_end:
+        transport.OneStep()
+
+    assert Integrate((transport.gfu - true_circle)**2, mesh)**(1/2) < 1e-2
+
+    transport.SetWind(-wind)
+
+    while transport.time < 2 * T_end:
+        transport.OneStep()
+
+    t.Set(0)
+    assert Integrate((transport.gfu - true_circle)**2, mesh)**(1/2) < 1e-2
+
+
+
+
+
+
