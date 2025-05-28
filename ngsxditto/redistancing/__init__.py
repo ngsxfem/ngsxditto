8fe8afbd5d4b0e0e16470a102d3ceaf63b68c09d
Mika Meyer <mika_meyer@gmx.de>
2025-05-28 21:19:56 +0200

    added autoredistancing

--- ngsxditto/redistancing/__init__.py
+++ ngsxditto/redistancing/__init__.py
@@ -2,3 +2,4 @@ from .redistancing import *
 from .linear_fmm import *
 from .quadratic_fmm import *
 from .fast_marching import *
+from .auto_redistancing import *
--- ngsxditto/redistancing/auto_redistancing.py
+++ ngsxditto/redistancing/auto_redistancing.py
@@ -1,17 +1,25 @@
-
-
-
 class AutoRedistancing:
     def __init__(self):
         pass
 
-    def ShouldRedistance(self):
+    def ShouldRedistance(self, levelset):
         raise NotImplementedError("ShouldRedistance not implemented")
 
 
 class PeriodicRedistancing(AutoRedistancing):
     def __init__(self, n):
+        super().__init__()
         self.n = n
 
-    def ShouldRedistance(self, step):
-        return step % self.interval == 0
+
+    def ShouldRedistance(self, levelset):
+        return levelset.steps_since_last_redistancing % self.n == 0
+
+class GradientRedistancing(AutoRedistancing):
+    def __init__(self, gradient_bounds):
+        super().__init__()
+        self.gradient_bounds = gradient_bounds
+
+    def ShouldRedistance(self, levelset):
+        min_grad, max_grad = levelset.MinMaxGradientNorm()
+        return self.gradient_bounds[0] >= min_grad and self.gradient_bounds[1] <= max_grad
