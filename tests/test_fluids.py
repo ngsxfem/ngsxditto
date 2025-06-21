6afc0856c72ec3a72fe6467a895af1f681d069ef
Mika Meyer <mika_meyer@gmx.de>
2025-06-21 17:36:14 +0200

    adapted to new structure

--- tests/test_fluids.py
+++ tests/test_fluids.py
@@ -1,4 +1,4 @@
-from ngsxditto.fluid import H1ConformingFluid, FluidParameters
+from ngsxditto.fluid import TaylorHood, FluidParameters
 from ngsolve import *
 
 
@@ -6,7 +6,7 @@ def test_h1conf_import():
     maxh = 0.2
     mesh = Mesh(unit_square.GenerateMesh(maxh=maxh))
     fluid_params = FluidParameters(viscosity=1e-3)
-    fluid = H1ConformingFluid(mesh, fluid_params=fluid_params)
+    fluid = TaylorHood(mesh, fluid_params=fluid_params)
 
 
 def test_h1conf_stokessolution():
@@ -16,11 +16,12 @@ def test_h1conf_stokessolution():
     order = 4
 
     fluid_params = FluidParameters(viscosity=1)
-    fluid = H1ConformingFluid(mesh, order=order, fluid_params=fluid_params)
+    fluid = TaylorHood(mesh, order=order, fluid_params=fluid_params)
 
     uin = CF((4*y*(1-y),0)) # parabolic inflow
-    fluid.SetBoundaryCondition(Dbndc=uin, Dbnd="left|bottom|top")
-    fluid.InitializeVarForm(rhs=CF((1,0)))
+    fluid.SetBoundaryConditions(dirichlet={"left|bottom|top": uin})
+    fluid.InitializeSpaces(dbnd="left|bottom|top")
+    fluid.InitializeForms(rhs=CF((1,0)))
 
     gfu = fluid.SolveStokes()
 
@@ -28,14 +29,7 @@ def test_h1conf_stokessolution():
     gfu_exact = GridFunction(fes)
     gfu_exact.Set(uin) # interpolated exact solution
 
-    Draw(gfu.components[0])
-
     l2_error = Integrate((gfu.components[0] - gfu_exact)**2, mesh)
     print("velocity L2-error: ", l2_error, " (compared to best approximation)")
 
     assert l2_error < 1e-9 
-
-
-if __name__=="__main__":
-    test_h1conf_import()
-    test_h1conf_stokessolution()
