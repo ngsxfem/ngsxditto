8b015f11cb8055b640cdff8419f2bb271f1cffc0
Mika Meyer <mika_meyer@gmx.de>
2025-06-28 22:47:10 +0200

    test all stokes discretizations

--- tests/test_fluids.py
+++ tests/test_fluids.py
@@ -1,35 +1,25 @@
-from ngsxditto.fluid import TaylorHood, FluidParameters
+from ngsxditto.fluid import *
 from ngsolve import *
+import pytest
 
 
-def test_h1conf_import():
-    maxh = 0.2
-    mesh = Mesh(unit_square.GenerateMesh(maxh=maxh))
-    fluid_params = FluidParameters(viscosity=1e-3)
-    fluid = TaylorHood(mesh, fluid_params=fluid_params)
+maxh = 0.1
+mesh = Mesh(unit_square.GenerateMesh(maxh=maxh))
+order = 4
+fluid_params = FluidParameters(viscosity=1)
+uin = CF((4 * y * (1 - y), 0))  # parabolic inflow
 
 
-def test_h1conf_stokessolution():
-    maxh = 0.1
-    mesh = Mesh(unit_square.GenerateMesh(maxh=maxh))
+@pytest.mark.parametrize("fluid_type", [TaylorHood, ScottVogelius, BDMHDG])
+def test_stokes(fluid_type):
+    fluid = fluid_type(mesh, order=order, fluid_params=fluid_params)
 
-    order = 4
-
-    fluid_params = FluidParameters(viscosity=1)
-    fluid = TaylorHood(mesh, order=order, fluid_params=fluid_params)
-
-    uin = CF((4*y*(1-y),0)) # parabolic inflow
     fluid.SetBoundaryConditions(dirichlet={"left|bottom|top": uin})
     fluid.InitializeSpaces(dbnd="left|bottom|top")
     fluid.InitializeForms(rhs=CF((1,0)))
 
     gfu = fluid.SolveStokes()
 
-    fes = VectorH1(mesh, order=order)
-    gfu_exact = GridFunction(fes)
-    gfu_exact.Set(uin) # interpolated exact solution
-
-    l2_error = Integrate((gfu.components[0] - gfu_exact)**2, mesh)
-    print("velocity L2-error: ", l2_error, " (compared to best approximation)")
-
-    assert l2_error < 1e-9 
+    l2_error = Integrate((gfu.components[0] - uin)**2, mesh)
+    print("velocity L2-error: ", l2_error)
+    assert l2_error < 1e-9
