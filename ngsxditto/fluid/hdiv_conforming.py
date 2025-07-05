66167ece54bdd2237ea5ca722bf069d61dccaa1b
Mika Meyer <mika_meyer@gmx.de>
2025-07-05 20:40:13 +0200

    minor changes

--- ngsxditto/fluid/hdiv_conforming.py
+++ ngsxditto/fluid/hdiv_conforming.py
@@ -2,11 +2,10 @@ from ngsolve import *
 
 from .params import FluidParameters, WallParameters
 from .discretization import FluidDiscretization
-from .. import LevelSetGeometry
 
 
 class HDivConforming(FluidDiscretization):
-    def __init__(self, mesh, fluid_params: FluidParameters, order=4, levelset:LevelSetGeometry=None, wall_params: WallParameters=None,
+    def __init__(self, mesh, fluid_params: FluidParameters, order=4, levelset=None, wall_params: WallParameters=None,
                  dt=None,  lamb:int=None):
         if levelset is not None:
             raise NotImplementedError("HDivConforming does not support unfitted functionality. Use H1-conforming elements instead.")
