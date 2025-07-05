030cd345970f723f764199a4503cf88b723d9861
Mika Meyer <mika_meyer@gmx.de>
2025-07-05 18:31:39 +0200

    adapted to callback, add cutinfo

--- ngsxditto/levelset/levelset.py
+++ ngsxditto/levelset/levelset.py
@@ -1,48 +1,85 @@
+from ngsxditto.callback import OnUpdateCallbacks
 from ngsxditto.transport import *
 from ngsxditto.redistancing import *
-import types
+from xfem import *
+from ngsolve import *
+#import types
 
 
-class LevelSetGeometry:
+class LevelSetGeometry(OnUpdateCallbacks):
     """
     This class handles the level set geometry.
     """
-    def __init__(self, transport: BaseTransport, redistancing: BaseRedistancing=None, autoredistancing: AutoRedistancing=None):
+    def __init__(self, transport: BaseTransport, redistancing: BaseRedistancing=None,
+                 autoredistancing: AutoRedistancing=None, initial_levelset=None):
         """
         Initializes the level set object with a transport method, a redistancing method and optionally an autoredistancing scheme.
         """
+        super().__init__()
         self.transport = transport
+        self.multistepper = MultiStepper()
+        self.multistepper.SetObject(self)
         if redistancing is not None:
             self.redistancing = redistancing
             self.redistancing.SetOrder(transport.order)
         self.mesh = self.transport.mesh
         self.autoredistancing = autoredistancing
         self.steps_since_last_redistancing = 0
+        self.AddCallback(self.RedistanceIfNecessary)
+        self.AddCallback(self.UpdateLinearApproximation)
+        self.AddCallback(self.UpdateCutInfo)
+        self.AddCallback(self.UpdateIntegrators)
 
-        original_func = self.transport.OneStep
-        # wrapper to track calls (useful for periodic auto redistancing)
-        def wrapped_OneStep(transport):
-            self.steps_since_last_redistancing += 1
-            original_func()
-            if self.ShouldRedistance():
-                self.Redistance()
+        P1 = H1(self.mesh, order=1)
+        self.lsetp1 = GridFunction(P1)
 
-        self.transport.OneStep = types.MethodType(wrapped_OneStep, self.transport)
+        self.ci = None
+        self.hasif = None
+        self.hasneg = None
+        self.haspos = None
+        self.any = None
+        self.dx_neg = None
+        self.dx_pos = None
+        self.dS = None
+
+        if initial_levelset is not None:
+            self.Initialize(initial_levelset)
+
 
     def SetRedistancing(self, redistancing: BaseRedistancing):
         self.redistancing = redistancing
 
     def Initialize(self, initial_lset: CoefficientFunction, initial_time: float=0.0):
         self.transport.SetInitialValues(initial_lset, initial_time)
+        self.UpdateLinearApproximation()
+        self.UpdateCutInfo()
+        self.UpdateIntegrators()
+
+
+    def UpdateLinearApproximation(self):
+        InterpolateToP1(self.field, self.lsetp1)
+
+    def UpdateCutInfo(self):
+        self.ci = CutInfo(self.mesh, self.lsetp1)
+        self.hasif = self.ci.GetElementsOfType(IF)
+        self.hasneg = self.ci.GetElementsOfType(HASNEG)
+        self.haspos = self.ci.GetElementsOfType(HASPOS)
+        self.any = self.ci.GetElementsOfType(ANY)
+
+    def UpdateIntegrators(self):
+        self.dx_neg = dCut(levelset=self.lsetp1, domain_type=NEG, definedonelements=self.hasneg)
+        self.dx_pos = dCut(levelset=self.lsetp1, domain_type=POS, definedonelements=self.haspos)
+        self.dS = dCut(levelset=self.lsetp1, domain_type=IF, definedonelements=self.hasif)
 
     def OneStep(self):
         self.transport.OneStep()
+        self.ProcessCallbacks()
 
     def RunFixedSteps(self, n):
-        self.transport.multistepper.RunFixedSteps(n)
+        self.multistepper.RunFixedSteps(n)
 
     def RunUntilTime(self, end_time):
-        self.transport.multistepper.RunUntilTime(end_time)
+        self.multistepper.RunUntilTime(end_time)
 
     def ShouldRedistance(self):
         if self.autoredistancing is not None:
@@ -52,24 +89,13 @@ class LevelSetGeometry:
 
     def Redistance(self):
         print("The next function is redistanced")
-        self.redistancing.Redistance(self.transport.field)
+        self.redistancing.Redistance(self.field)
         self.steps_since_last_redistancing = 0
 
+    def RedistanceIfNecessary(self):
+        if self.ShouldRedistance():
+            self.Redistance()
 
-    def MinMaxGradientNorm(self, bandwidth=None):
-        phi = self.transport.field
-        norm_grad = Norm(grad(phi))
-        V = phi.space
-        gfu = GridFunction(V)
-        gfu.Set(norm_grad)
-
-        max_grad = -1e100
-        min_grad = 1e100
-
-        for v in self.mesh.vertices:
-            point = self.mesh[v].point
-            val = gfu(self.mesh(*point))  # this is not scale well in terms of evaluations
-            max_grad = max(max_grad, val)
-            min_grad = min(min_grad, val)
-
-        return min_grad, max_grad
+    @property
+    def field(self):
+        return self.transport.field
