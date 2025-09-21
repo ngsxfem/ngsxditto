6f66a91684a33a18ebfbb17e2bbc02a96c71017e
Mika Meyer <mika_meyer@gmx.de>
2025-09-21 12:31:08 +0200

    dummy

---
+++ ngsxditto/stateholder.py
@@ -0,0 +1,19 @@
+
+
+class Stateholder:
+    def __init__(self):
+        pass
+
+    def BeforeLoop(self):
+        pass
+
+    def AfterLoop(self):
+        pass
+
+    def Advance(self):
+        pass
+
+    def StoreState(self):
+        pass
+
+    
