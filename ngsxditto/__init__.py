6ba68b5e6cac973ba6f1f173dda6b52ca4a4cc95
Mika Meyer <mika_meyer@gmx.de>
2025-10-07 11:30:27 +0200

    add velocity extension

--- ngsxditto/__init__.py
+++ ngsxditto/__init__.py
@@ -9,6 +9,7 @@ from .levelset import *
 from .redistancing import *
 from .multistepper import *
 from .gradient_tester import *
+from .velocity_extension import *
 from .callback import *
 from .solver import *
 from .visualization import *
