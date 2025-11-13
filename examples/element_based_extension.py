991ebd2230f3da1aafa33a3290e764b4f78fe356
Paul Schwering <schwering@igpm.rwth-aachen.de>
2025-11-13 17:10:28 +0100

    renamed extension test file

--- examples/element_based_extension.py
+++ examples/element_based_extension.py
@@ -1,57 +1,60 @@
 #%%
-from ngsolve import * 
+# imports
+from ngsolve import *
 
 from ngsolve import unit_square, Mesh, CoefficientFunction, CF
 
 from ngsolve.webgui import *
 
+from ngsxditto.extension import *
+
+from xfem.utils import AdjacencyMatrix, AddNeighborhood
+
 from time import sleep
 #%%
+# mesh and gridfunctions
 mesh = Mesh(unit_square.GenerateMesh(maxh=0.02))
-
-#%%
-from ngsxditto.extension import *
-from xfem.utils import AdjacencyMatrix, AddNeighborhood
-V = H1(mesh,order=5, dgjumps=True, dirichlet=".*")
+V = H1(mesh, order=5, dgjumps=True, dirichlet=".*")
 gfu = GridFunction(V)
-gfw = GridFunction(V)
-
 
 lsetp1 = GridFunction(H1(mesh,order=1))
 lsetp1.Set(sqrt((x-0.5)**2 + (y-0.5)**2) - 0.3)
-
 ci = CutInfo(mesh, lsetp1)
+
+#%%
+# element markers
 negels = ci.GetElementsOfType(NEG)
 ifels = ci.GetElementsOfType(IF)
 hasneg = ci.GetElementsOfType(HASNEG)
 haspos = ci.GetElementsOfType(HASPOS)
 anyelems = ci.GetElementsOfType(ANY)
+
 #%%
-
-#band = BitArray(mesh.ne)
+# oldband = elems where function is defined previously, band = elems where function is defined after extension
+oldband = ifels
 band = AddNeighborhood(ifels, AdjacencyMatrix(mesh,"vertex"), layers=3)
-#ifels = ci.GetElementsOfType(IF)
 
-
+# input function defined on first oldband
 theta = atan2(y-0.5,x-0.5)
-gfu.Set(sin(theta), definedonelements=ifels)
+gfu.Set(sin(theta), definedonelements=oldband)
+#gfu.Set(sin(4*pi*x*y) , definedonelements=oldband)
 
-#gfu.Set(sin(4*pi*x*y) , definedonelements=ifels)
-
-u,v = V.TnT()
+# choose normal volume energy form for extension
+u, v = V.TnT()
 n = Normalize(grad(lsetp1))
 energyform = (grad(u)|n)*(grad(v)|n)*dx(definedonelements=band)
 
-oldband = ifels
-
-ebext = ElementBasedExtension(gfu, oldband, band, 
+ebext = ElementBasedExtension(gfu, oldband, band,
                               energyform=energyform,
                               activeelems=band,
                               activefacets=None
                             )
+# # alternatively, use default energy form (ghost penalty)
+# ebext = ElementBasedExtension(gfu, oldband, band)
 
-                              #dirichlet_dofs=~V.FreeDofs())
+
 #%%
+# store output in multidim gridfunction
 gfwvis = GridFunction(V, multidim=0)
 gfwvis.AddMultiDimComponent(gfu.vec)
 ebext.Step()
@@ -62,5 +65,4 @@ for i in range(20):
     ebext.Step()
     gfwvis.AddMultiDimComponent(gfu.vec)
 Draw(gfwvis, mesh, "gfwvis")
-
 # %%
--- examples/narrow_band_transport.py
+++ examples/narrow_band_transport.py
@@ -1,32 +1,35 @@
 #%%
-from ngsolve import * 
+from ngsolve import *
 
 from ngsolve import unit_square, Mesh, CoefficientFunction, CF
+from ngsolve.webgui import *
 
-from time import sleep
-#%%
-mesh = Mesh(unit_square.GenerateMesh(maxh=0.07))
-
-#%%
 from ngsxditto.levelset import *
-from ngsxditto.solver import *
-from ngsxditto.extension import *
+# from ngsxditto.solver import *
+# from ngsxditto.extension import *
 #from ngsxditto.transport import *
+from xfem.utils import AdjacencyMatrix, AddNeighborhood
 
+# from time import sleep
+#%%
+# mesh and parameters
+mesh = Mesh(unit_square.GenerateMesh(maxh=0.07))
 dt = 0.01
 t = Parameter(0)
+adj = AdjacencyMatrix(mesh,"vertex")
 
+#%%
 wind = IfPos(t-2,1,-1)*CF((0.5-y, x-0.5))
 #wind = CF((-1,0))
 inflow_values = CF(0)
 transport_elems = BitArray(mesh.ne)
 transport_elems[:] = True
 support_elems = BitArray(mesh.ne)
-transport = ImplicitDGTransport(mesh, wind, inflow_values, dt, 
-                                order=1, active_elements=transport_elems)
+transport = ImplicitDGTransport(mesh, wind, inflow_values, dt,
+								order=1, active_elements=transport_elems)
 levelset = LevelSetGeometry(transport)
 #%%
-
+# set initial values for the level set function
 #levelset.Initialize(exp((-4*((x-0.6)**2+(y-0.5)**2)))-0.8, initial_time = 0)
 levelset.Initialize(sqrt((x-0.6)**2+(y-0.5)**2) - 0.15, initial_time = 0)
 #%%
@@ -34,14 +37,12 @@ Draw(transport.past, mesh, "udisc_past", min=-0.001, max=0.001, autoscale=False)
 Draw(transport.field, mesh, "udisc", min=-0.001, max=0.001, autoscale=False)
 Draw(levelset.field, mesh, "ucont", min=-0.001, max=0.001, autoscale=False)
 
-#%
-from xfem.utils import AdjacencyMatrix, AddNeighborhood
-adj = AdjacencyMatrix(mesh,"vertex")
+#%%
 def UpdateElemMarker():
-    support_elems[:] = AddNeighborhood(levelset.hasif, adj, layers=1, inplace=False)
-    #support_elems &= transport_elems
-    transport_elems[:] = AddNeighborhood(levelset.hasif, adj, layers=3, inplace=False)
-    
+	support_elems[:] = AddNeighborhood(levelset.hasif, adj, layers=1, inplace=False)
+	# support_elems &= transport_elems
+	transport_elems[:] = AddNeighborhood(levelset.hasif, adj, layers=3, inplace=False)
+
 target_elems = BitArray(mesh.ne)
 target_elems[:] = True
 
@@ -49,12 +50,13 @@ ebext = ElementBasedExtension(levelset.transport.past, support_elems, transport_
 #ebext = ElementBasedExtension(levelset.transport.past, support_elems, target_elems)
 
 #input("")
-def redraw():
-    #sleep(0.01)
-    Redraw(blocking=True)
-    #input("")
 
 #%%
+# create time loop
+
+def redraw():
+	Redraw(blocking=True)
+	#input("")
 
 time_loop = TimeLoop(time=t, dt=dt, end_time=4)
 time_loop.Register(UpdateElemMarker, name="udpate transport elements")
@@ -63,6 +65,7 @@ time_loop.Register(levelset, name="levelset")
 time_loop.Register(redraw, name="redraw")
 
 
-
 # %%
+# run time loop
 time_loop()
+# %%
