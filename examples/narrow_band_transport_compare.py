6394b97953b1a04dadc8f9ba4b0d1648bfff21ec
Paul Schwering <schwering@igpm.rwth-aachen.de>
2026-02-10 13:51:06 +0100

    higher order is working with cont old lset

--- examples/narrow_band_transport_compare.py
+++ examples/narrow_band_transport_compare.py
@@ -11,8 +11,8 @@ from ngsolve.webgui import *
 # parameters and mesh
 plot_height = 600
 
-kg = 1
-dt = 0.1
+kg = 3
+dt = 0.05
 h = 0.07
 layers_proj = 1
 
@@ -21,29 +21,26 @@ AdjaMat = AdjacencyMatrix(mesh, neighbortype="vertex")
 t = Parameter(0)
 wind = IfPos(t-2,1,-1)*CF((0.5-y, x-0.5))
 
-# compare values
-value_ds = 0
-value_dx = 0
-
 #%%
-
+# aux gridfunctions
 VGammaGF = GridFunction(H1(mesh, order=1), autoupdate=True) # to define delta for extension
+transport_past_cont = GridFunction(H1(mesh, order=kg, dgjumps=True))
 
 # markers
 transport_elems, old_transport_elems, support_elems = BitArray(mesh.ne), BitArray(mesh.ne), BitArray(mesh.ne)
 transport_elems[:] = True
 old_transport_elems[:] = True
+support_dofs = BitArray(transport_past_cont.space.ndof)
 
 # transport class
 transport = ImplicitDGTransport(mesh, wind=wind, dt=dt, order=kg, active_elements=transport_elems)
 levelset = LevelSetGeometry(transport)
-fes_old_lset = transport.current.space
-support_dofs = BitArray(fes_old_lset.ndof)
+lset_ext = ElementBasedExtension(gfs=[transport_past_cont], supportelems=support_elems, targetelems=transport_elems, dirichlet_dofs=support_dofs)
 # lset_ext = ElementBasedExtension(gfs=[levelset.transport.past], supportelems=support_elems, targetelems=transport_elems)
-lset_ext = ElementBasedExtension(gfs=[levelset.transport.past], supportelems=support_elems, targetelems=transport_elems, dirichlet_dofs=support_dofs)
 
 # set initial values for the level set function
 levelset.Initialize(sqrt((x-0.6)**2+(y-0.5)**2) - 0.15, initial_time = 0)
+transport_past_cont.Set(sqrt((x-0.6)**2+(y-0.5)**2) - 0.15)
 t.Set(0)
 
 #%%
@@ -59,17 +56,16 @@ def UpdateElemMarker():
 	# update element and dof markers
 	transport_elems[:] = AddNeighborhood(levelset.hasif, AdjaMat, layers=layers_trans)
 	support_elems[:] = AddNeighborhood(levelset.hasif, AdjaMat, layers=layers_proj) & old_transport_elems & transport_elems
-	support_dofs[:] = GetDofsOfElements(fes_old_lset, support_elems)
+	support_dofs[:] = GetDofsOfElements(transport_past_cont.space, support_elems)
+
+def InterExt():
+	levelset.transport.past.Set(transport_past_cont)
 
 def Finish():
-	global value_ds, value_dx
-
 	# overwrite last element markers
 	old_transport_elems[:] = transport_elems
+	transport_past_cont.Set(levelset.field)
 	cont_vis.AddMultiDimComponent(levelset.field.vec)
-
-	value_ds += sqrt(Integrate(CF(1) * levelset.dS, mesh))
-	value_dx += sqrt(Integrate(levelset.field**2 * dx(definedonelements=transport_elems), mesh))
 
 
 #%%
@@ -78,13 +74,12 @@ t.Set(0)
 time_loop = TimeLoop(time=t, dt=dt, end_time=4)
 time_loop.Register(UpdateElemMarker, name="udpate transport elements")
 time_loop.Register(lset_ext, name="element based level set extension")
+time_loop.Register(InterExt, name="set old level set")
 time_loop.Register(levelset, name="levelset")
 time_loop.Register(Finish, name="Finish")
 time_loop()
 
 # %%
-# final results
-print(f"Difference to old code = {np.abs(value_ds - 9.710888279890247) + np.abs(value_dx - 0.9283146818444067)}")
 Draw(cont_vis, mesh, height=f'{plot_height}px', width=f'{1.5*plot_height}px')
 
 # %%
