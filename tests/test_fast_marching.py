11850a811dc33d3517bc178a585a697ce8b12d46
Mika Meyer <mika_meyer@gmx.de>
2026-06-20 11:58:36 +0200

    deleted unnecessary files/tests

--- tests/test_fast_marching.py
+++ tests/test_fast_marching.py
@@ -11,12 +11,12 @@ circle = x**2 + y**2 - 0.25
 true_signed_distance = (x**2 + y**2)**(1/2) - 1/2
 
 
-def test_h1_global():
+def test_globally():
     V = H1(mesh, order=1)
     phi = GridFunction(V)
     phi.Set(circle)
 
-    redistancing = LinearFastMarching()
+    redistancing = FastMarching()
     redistancing.Redistance(phi)
 
     assert Integrate((phi - true_signed_distance)**2, mesh)**(1/2) < 1e-1  # L2-error
@@ -26,7 +26,7 @@ def test_bandwidth():
     phi = GridFunction(V)
     phi.Set(circle)
 
-    redistancing = LinearFastMarching(bandwidth=0.5)
+    redistancing = FastMarching(bandwidth=0.5)
     redistancing.Redistance(phi)
     norm_grad = Norm(grad(phi))
     assert abs(norm_grad(mesh(0, 0)) - 1) > 0.5
@@ -34,15 +34,6 @@ def test_bandwidth():
     assert abs(norm_grad(mesh(0.7, 0)) - 1) < 0.15
     assert abs(norm_grad(mesh(1, 0)) - 1) > 0.5
 
-def test_l2_global():
-    V = L2(mesh, order=1, dgjumps=True)
-    phi = GridFunction(V)
-    phi.Set(circle)
-
-    redistancing = LinearFastMarching()
-    redistancing.Redistance(phi)
-
-    assert Integrate((phi - true_signed_distance)**2, mesh)**(1/2) < 1e-1  # L2-error
 
 
 
