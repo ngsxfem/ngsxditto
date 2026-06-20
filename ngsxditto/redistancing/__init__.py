c9146fc3829a8aff29c81a988fe1d5914a729f9a
Mika Meyer <mika_meyer@gmx.de>
2026-06-20 12:00:29 +0200

    delete deleted files

--- ngsxditto/redistancing/__init__.py
+++ ngsxditto/redistancing/__init__.py
@@ -3,8 +3,6 @@ logger = logging.getLogger(__name__)
 logger.debug("importing ngsxditto.redistancing")
 
 from .redistancing import *
-from .linear_fmm import *
-from .quadratic_fmm import *
 from .fast_marching import *
 from .eikonal import *
 from .auto_redistancing import *
